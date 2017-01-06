b40c949ccba588f60b212a6251561bdad2e9660c
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2017-01-06 10:27:44 +0800

    OOP-HW1

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,11 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics = "Let it go! Let it go!Cannot hold it back anymore ";
+		System.out.println(lyrics.replace("it","\"her\"").replace("Cannot", "Can't"));
+	}
+
+}
